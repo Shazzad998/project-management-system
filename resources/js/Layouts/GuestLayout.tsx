7407e8c9e7c6d34b35d6577a538e17b5ee2e16fe
Shazzad998 <shazzadulislam998@gmail.com>
2024-08-26 19:12:40 +0600

    dark mood added

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react';
 
 export default function Guest({ children }: PropsWithChildren) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-background">
             <div>
                 <Link href="/">
                     <ApplicationLogo className="w-16 h-16 fill-current text-foreground" />
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -7,8 +7,8 @@ export default function Dashboard({ auth }: PageProps) {
         <AuthenticatedLayout user={auth.user}>
             <Head title="Dashboard" />
 
-            <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div className="p-6 text-gray-900">You're logged in!</div>
+            <div className="bg-background  overflow-hidden shadow-sm sm:rounded-lg">
+                <div className="p-6">You're logged in!</div>
             </div>
         </AuthenticatedLayout>
     );
